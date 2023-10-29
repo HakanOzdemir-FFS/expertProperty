9689a55370c2713844c68bdca57e5a06cfb24335
Hakan Ozdemir <hak.ozdmr@hotmail.com>
2023-10-30 00:50:13 +0300

    footer-fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,15 @@ module.exports = {
         "custom-gray-3": "#5B5652",
         "custom-gray-4": "#F2F2F2",
       },
+
       backgroundImage: {
         "custom-gradient":
           "linear-gradient(0deg, #FFF6AF 0%, #C7923E 33%, #DBB667 66%, #C5822A 100%)",
         "custom-gradient-2":
           "linear-gradient(139.94deg, #FFF6AF -11.33%, #C7923E -11.33%, #DBB667 49.37%, #C5822A 100%)",
+      },
+      screens: {
+        lg: "1200px",
       },
     },
   },
